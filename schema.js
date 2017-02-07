a9a82f70854221093a3bee9fbffabbddda3c3d83
Toby Applegate <up661724@myport.ac.uk>
2017-02-07 21:02:41 +1100

    changed connection function

--- schema.js
+++ schema.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env nodejs
 
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/users')
 
-var db = mongoose.connect('mongodb://localhost/users');
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("connected to database successfully!");
